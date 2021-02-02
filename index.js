b465130e3ff803a1cac5f0936aeea912f81eaf10
Mathias Buus <mathiasbuus@gmail.com>
2021-02-02 14:26:10 +0100

    pass in allocer to tree file

--- index.js
+++ index.js
@@ -9,8 +9,14 @@ try {
 module.exports = defaultStorage
 
 function defaultStorage (name, opts) {
+  // make it easier to cache tree nodes without the big unsafe arraybuffer attached
+  if (isTree(name)) return new RAF(name, { alloc: Buffer.alloc, ...opts })
   if (!isBitfield(name)) return new RAF(name, opts)
   return new RAF(name, { lock, ...opts })
+}
+
+function isTree (name) {
+  return name === 'tree' || name.endsWith('/tree')
 }
 
 function isBitfield (name) {
