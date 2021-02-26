83257ce0b50683a6e43d45ac2fdfee55724b6bd6
Mathias Buus <mathiasbuus@gmail.com>
2021-02-26 17:35:31 +0100

    use fsctl to enable cross platform sparse files

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const RAF = require('random-access-file')
 
 let lock = null
+let sparse = null
 
 try {
-  lock = require('fd-lock')
+  const fsctl = require('fsctl')
+  lock = fsctl.lock
+  sparse = fsctl.sparse
 } catch (_) {}
 
 module.exports = defaultStorage
 
 function defaultStorage (name, opts) {
   // make it easier to cache tree nodes without the big unsafe arraybuffer attached
-  if (isTree(name)) return new RAF(name, { alloc: Buffer.alloc, ...opts })
-  if (!isBitfield(name)) return new RAF(name, opts)
-  return new RAF(name, { lock, ...opts })
+  if (isTree(name)) return new RAF(name, { sparse, alloc: Buffer.alloc, ...opts })
+  if (!isBitfield(name)) return new RAF(name, { sparse, ...opts })
+  return new RAF(name, { lock, sparse, ...opts })
 }
 
 function isTree (name) {
